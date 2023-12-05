4d52e099cea162b0da51540a1936fb05376a5e44
iRxllz <vrollandvass@yahoo.com>
2023-12-05 22:21:38 +0200

    Feature/BTS-1 , Check FIXME.md

---
+++ FIXME.md
@@ -0,0 +1,6 @@
+Feature BTS-1
+
+Tournament feature added (Winner fights next)
+
+Problem:
+Characters keep their Health and Armor from the old fight.
--- src/App.java
+++ src/App.java
@@ -13,40 +13,47 @@ public class App {
         // - randomized critical strike (15% chance),
         // - randomized parries (35% chance)
 
-        // Archer archer1 = new Archer(new Pair(4, 8), 0);
-        // Goblin goblin1 = new Goblin(new Pair(7, 9), 0);
-
-        // combatManager.showBattleInfo(archer1, goblin1);
-        // fight(archer1, goblin1);
-
+        Archer archer1 = new Archer(new Pair(4, 8), 0);
+        Goblin goblin1 = new Goblin(new Pair(7, 9), 0);
         Knight knight1 = new Knight(new Pair(3, 4), 6);
         Ogre ogre1 = new Ogre(new Pair(5, 6), 4);
 
         combatManager.showBattleInfo(knight1, ogre1);
-        fight(knight1, ogre1);
+        Character winner1 = fight(knight1, ogre1);
+
+        combatManager.showBattleInfo(winner1, archer1);
+        Character winner2 = fight(winner1, archer1);
+
+        combatManager.showBattleInfo(winner2, goblin1);
+        fight(winner2, goblin1);
 
     }
 
-    public static void fight(Character attacker, Character defender)
+    public static Character fight(Character attacker, Character defender)
             throws InterruptedException {
+        Character winner = null;
+
         if (attacker.getHealth() > 0 && defender.getHealth() > 0) {
             combatManager.attack(attacker, defender);
             System.out.println(defender);
             System.out.println();
             TimeUnit.MILLISECONDS.sleep(250);
 
-            fight(defender, attacker);
+            winner = fight(defender, attacker);
 
         } else {
             if (attacker.getHealth() <= 0) {
+                winner = defender;
                 System.out.println(GREEN + defender.getClass().getName() + " won with " + defender.getHealth()
-                        + " health and " + defender.getArmor() + " armor remaining!" + RESET);
+                        + " health and " + defender.getArmor() + " armor remaining!\n" + RESET);
             }
             if (defender.getHealth() <= 0) {
+                winner = attacker;
                 System.out.println(GREEN + attacker.getClass().getName() + " won with " + attacker.getHealth()
-                        + " health and " + attacker.getArmor() + " armor remaining!" + RESET);
+                        + " health and " + attacker.getArmor() + " armor remaining!\n" + RESET);
             }
         }
+        return winner;
     }
 }
 
